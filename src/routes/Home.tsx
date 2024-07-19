c59163d3480385a9cc3ed6236fea23d406a7c70f
Dhemeira <peterlakics2@gmail.com>
2024-07-19 10:47:47 +0200

    Remove lorem ipsum

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -39,64 +39,7 @@ export const Home = () => {
         </div>
 
         <div className="row">
-          <div className="col">
-            <h2 className="text-3xl leading-loose">Lorem ipsum</h2>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto sequi veniam
-              voluptatibus eveniet deserunt aperiam nostrum ducimus eaque dicta et doloribus aut
-              maiores dolores exercitationem corporis facilis debitis, molestiae cumque!
-            </p>
-            <p>
-              Delectus soluta culpa commodi sit earum libero minus adipisci ullam nobis praesentium
-              dolorum inventore distinctio, illo facere velit quos in, amet voluptatum corporis
-              quas, esse nam natus enim iusto. Molestiae.
-            </p>
-            <p>
-              Numquam molestias modi laudantium neque perspiciatis ipsa expedita doloremque enim?
-              Deleniti, necessitatibus adipisci? Laboriosam nesciunt nihil, molestiae saepe quos
-              impedit, ipsa sunt ea exercitationem magnam esse itaque pariatur obcaecati vero!
-            </p>
-            <p>
-              Reprehenderit modi odio facilis ullam ea nesciunt deserunt excepturi quidem ducimus
-              minima animi iure impedit vero ut molestias ipsum ipsa, eum provident consequatur a.
-              Ipsam animi expedita distinctio nam possimus.
-            </p>
-            <p>
-              Fuga, repudiandae dolore quas officiis eveniet similique aliquam totam aut temporibus
-              veniam quos harum eius eum animi. Assumenda officia, repellendus obcaecati omnis,
-              architecto cupiditate fuga rem aut, est modi perspiciatis.
-            </p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <h2 className="text-3xl leading-loose">Lorem ipsum</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque reprehenderit
-              sapiente quis impedit officia vel quidem dignissimos. Natus ex, iure totam architecto
-              rerum, pariatur delectus corrupti saepe magni nostrum neque!
-            </p>
-            <p>
-              Saepe, ullam? Ex porro consectetur cupiditate ipsum fuga suscipit laboriosam adipisci
-              eius provident consequuntur, sit, quibusdam alias qui laudantium amet voluptas
-              voluptatibus molestias voluptate vel obcaecati officia, quisquam quam nihil!
-            </p>
-            <p>
-              Nemo veritatis perspiciatis doloremque eos aperiam beatae a? Facere deserunt omnis
-              obcaecati nemo voluptatibus officiis atque fugit consequuntur porro debitis! Officia
-              odio repellat deleniti dolor distinctio quae perspiciatis assumenda qui?
-            </p>
-            <p>
-              Quis optio possimus mollitia quidem consequatur quas hic ex eum, nam id necessitatibus
-              est expedita similique quasi earum totam numquam ad saepe laboriosam tenetur veniam
-              repellat cupiditate voluptates distinctio? Error.
-            </p>
-            <p>
-              Dolorum sequi perspiciatis nostrum. Illo voluptas tempore consectetur aliquam velit
-              quisquam laudantium magni ipsum sed corrupti possimus reprehenderit non quidem
-              temporibus ipsam, voluptates ab sapiente incidunt enim perspiciatis obcaecati dolores!
-            </p>
-          </div>
+          <div className="col"></div>
         </div>
       </div>
     </>
