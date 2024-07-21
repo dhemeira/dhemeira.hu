2a413e2b9a410a3c1826c57c9bee41b337d4197a
dhemeira <peterlakics2@gmail.com>
2024-07-21 13:27:59 +0200

    Change title, header and footer branding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ export const Footer = () => {
   return (
     <footer className="bg-black w-full px-[5%] md:px-[10%] py-2 flex justify-between items-center text-dark-text border-t-white/20 border-t">
       <span>
-        © 2023-{new Date().getFullYear()} | <a href="https://dhemeira.hu">dhemeira.hu</a> - Péter
-        Lakics
+        © 2023-{new Date().getFullYear()} | <a href="https://dhemeira.hu">www.dhemeira.hu</a> -
+        Péter Lakics
       </span>
       <a
         href="https://github.com/dhemeira"
