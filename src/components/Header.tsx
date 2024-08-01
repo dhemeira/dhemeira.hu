0fbf3582f161262fc01ba1bcbcd66b045054abed
Dhemeira <peterlakics2@gmail.com>
2024-08-01 09:20:26 +0200

    Change site logo into navlink

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -58,7 +58,11 @@ export const Header = ({ changeTheme, theme }: HeaderProps) => {
   return (
     <header className="fixed top-0 w-full bg-black/75 z-50 backdrop-blur-xl text-dark-text border-b-white/20 border-b">
       <div className="flex justify-between px-[5%] md:px-[10%] py-2">
-        <p className="font-medium px-2 text-base/8">dhemeira</p>
+        <NavLink
+          className="font-medium px-2 text-base/8"
+          to="/">
+          dhemeira
+        </NavLink>
         <nav className="flex gap-4 justify-end items-center">
           {pages.map((page) => (
             <NavLink
