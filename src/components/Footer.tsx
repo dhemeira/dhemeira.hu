4ad3ddedad13958895860806dab4ba21e3cbcf8a
Dhemeira <peterlakics2@gmail.com>
2024-08-05 11:14:15 +0200

    Change footer fix #9

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,13 @@ import { BsGithub } from 'react-icons/bs';
 import React from 'react';
 export const Footer = () => {
   return (
-    <footer className="bg-black w-full px-[5%] md:px-[10%] py-2 flex justify-between items-center text-dark-text border-t-white/20 border-t">
-      <span>
-        © 2023-{new Date().getFullYear()} | <a href="https://dhemeira.hu">www.dhemeira.hu</a> -
-        Péter Lakics
+    <footer className="bg-black w-full px-[5%] md:px-[10%] py-2 flex justify-between items-end text-dark-text border-t-white/20 border-t">
+      <span className="flex flex-col">
+        <a href="https://dhemeira.hu">www.dhemeira.hu</a>
+        <span>© 2023-{new Date().getFullYear()} | Péter Lakics</span>
       </span>
       <a
+        className="p-1"
         href="https://github.com/dhemeira"
         aria-label="GitHub"
         title="GitHub"
