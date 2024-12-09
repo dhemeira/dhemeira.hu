62b56a151dd09e9d6e43f1ce9bde89fc59dac50d
dhemeira <peterlakics2@gmail.com>
2024-12-09 23:15:28 +0100

    fix font error

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import './styles/global.css';
-import '@fontsource-variable/inter';
+import '@fontsource-variable/inter/index.css';
 import App from './App.vue';
 
 createApp(App).mount('#app');
